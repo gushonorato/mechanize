f4eed49b40642386e31c1852ba3d35acb5add428
Gutavo Honorato <gustavohonorato@gmail.com>
2019-05-17 17:19:33 -0300

    Implemented DOM manipulation functions “with_elements”

--- lib/mechanizex/page.ex
+++ lib/mechanizex/page.ex
@@ -6,7 +6,8 @@ defmodule Mechanizex.Page do
   @type t :: %__MODULE__{
           request: Request.t(),
           response: Response.t(),
-          agent: pid()
+          agent: pid(),
+          links: list(Link.t())
         }
 
   def body(page) do
@@ -20,14 +21,75 @@ defmodule Mechanizex.Page do
   def html_parser(page) do
     page
     |> agent
-    |> Mechanizex.html_parser()
+    |> Mechanizex.Agent.html_parser()
   end
 
-  def links(page) do
+  defdelegate links(page), to: __MODULE__, as: :with_links
+
+  def with_links(page, criterias \\ []), do: with_elements(page, [:a, :area], criterias)
+
+  def with_elements(page, element_names, criterias \\ [])
+  def with_elements(page, element_names, criterias) do
     page
-    |> find("a")
-    |> Enum.into(find(page, "area"))
-    |> Enum.map(&Link.create/1)
+    |> maybe_filter_by_selector(criterias)
+    |> filter_by_element_names(element_names)
+    |> filter_by_criteria(criterias)
+  end
+
+  defp maybe_filter_by_selector(page, css: selector) do
+    search(page, selector)
+  end
+
+  defp maybe_filter_by_selector(page, _) do
+    page
+  end
+
+  defp filter_by_element_names(%Mechanizex.Page{} = page, names) do
+    names = Enum.map(names, &normalize/1)
+    Enum.flat_map(names, fn name -> search(page, name) end)
+  end
+
+  defp filter_by_element_names(elements, names) do
+    names = Enum.map(names, &normalize/1)
+    Enum.filter(elements, fn e -> e.name in names end)
+  end
+
+  defp filter_by_criteria(elements, criterias) do
+    criterias = Keyword.delete(criterias, :css)
+    Enum.filter(elements, &all_criterias_meet?(&1, criterias))
+  end
+
+  defp all_criterias_meet?(element, [h | t]) do
+    criteria_meet?(element, h) and all_criterias_meet?(element, t)
+  end
+
+  defp all_criterias_meet?(element, []) do
+    true
+  end
+
+  defp criteria_meet?(element, {attr, value}) when is_atom(attr) do
+    criteria_meet?(element, {normalize(attr), value})
+  end
+
+  defp criteria_meet?(element, {"text", value}) when is_binary(value) do
+    element.text == value
+  end
+
+  defp criteria_meet?(element, {"text", value}) do
+    element.text =~ value
+  end
+
+  defp criteria_meet?(element, {attr_name, value}) when is_binary(value) do
+    element.attributes[attr_name] == value
+  end
+
+  defp criteria_meet?(element, {attr_name, value}) do
+    attr_value = element.attributes[attr_name]
+    attr_value != nil and attr_value =~ value
+  end
+
+  defp normalize(name) do
+    if is_atom(name), do: Atom.to_string(name), else: name
   end
 
   defp delegate_parser(method, params) do
@@ -37,5 +99,5 @@ defmodule Mechanizex.Page do
     |> apply(method, params)
   end
 
-  def find(page, selector), do: delegate_parser(:find, [page, selector])
+  def search(page, selector), do: delegate_parser(:search, [page, selector])
 end
---
+++ test/mechanizex/page_test.exs
@@ -0,0 +1,128 @@
+defmodule Mechanizex.PageTest do
+  use ExUnit.Case, async: true
+  alias Mechanizex.Response
+  alias Mechanizex.Page
+  doctest Mechanizex.Page
+
+  @html """
+    <html>
+    <head>
+    <title id="elem_1">Test</title>
+    <meta id="elem_2" name="description" content="Test webpage"/>
+    </head>
+    <body>
+      <div id="elem_3" class="container" data-method="get">
+        <a id="elem_4" href="http://google.com.br" rel="nofollow" class="company js-google js-cool">Google Brazil</a>
+      </div>
+      <div id="elem_5" class="content">
+        <a id="elem_6" href="http://google.com" class="company js-google js-cool">Google</a>
+        <a id="elem_7" href="http://elixir-lang.org" class="js-elixir js-cool">Elixir lang</a>
+        <a id="elem_8" href="http://java.com" class="js-java">Java</a>
+      </div>
+    </body>
+    </html>
+  """
+
+  setup_all do
+    {:ok,
+     page: %Page{
+       agent: Mechanizex.new(),
+       response: %Response{
+         body: @html
+       }
+     }}
+  end
+
+  describe ".with_elements" do
+    defp with_elements_map(attr_for_map, page, elements, criteria \\ []) do
+      page
+      |> Page.with_elements(elements, criteria)
+      |> Enum.map(&Map.get(&1, attr_for_map))
+    end
+
+    test "invalid string criteria", state do
+      assert with_elements_map(:name, state.page, [:a], foo: "bar") == []
+    end
+
+    test "invalid regexp criteria", state do
+      assert with_elements_map(:name, state.page, [:title], foo: ~r/bar/) == []
+    end
+
+    test "one element returned", state do
+      assert with_elements_map(:name, state.page, [:title]) == ["title"]
+    end
+
+    test "criteria keys can be strings", state do
+      assert with_elements_map(:dom_id, state.page, [:a], [{"href", ~r/google.com.br/}]) == ["elem_4"]
+    end
+
+    test "element names can be strings", state do
+      assert with_elements_map(:dom_id, state.page, [:a], href: ~r/google.com.br/) == ["elem_4"]
+    end
+
+    test "css criteria", state do
+      assert with_elements_map(:dom_id, state.page, [:a], css: ".js-google") == [
+               "elem_4",
+               "elem_6"
+             ]
+    end
+
+    test "broad match with regex", state do
+      assert with_elements_map(:dom_id, state.page, [:a], href: ~r/google.com.br/) == ["elem_4"]
+    end
+
+    test "broad match with string won't work", state do
+      assert with_elements_map(:dom_id, state.page, [:a], href: "google.com.br") == []
+    end
+
+    test "exact match with string in criteria", state do
+      assert with_elements_map(:dom_id, state.page, [:a], href: "http://google.com.br") == [
+               "elem_4"
+             ]
+    end
+
+    test "accepts many attributes in criteria", state do
+      assert with_elements_map(:dom_id, state.page, [:a],
+               class: ~r/js-google /,
+               rel: ~r/nofollow/
+             ) == ["elem_4"]
+    end
+
+    test "broad match text with regexp", state do
+      assert with_elements_map(:dom_id, state.page, [:a], text: ~r/Google/) == [
+               "elem_4",
+               "elem_6"
+             ]
+    end
+
+    test "exact match text with string", state do
+      assert with_elements_map(:dom_id, state.page, [:a], text: "Google") == ["elem_6"]
+    end
+
+    test "text and many attributes criteria", state do
+      assert with_elements_map(:dom_id, state.page, [:a],
+               text: ~r/Google Brazil/,
+               class: ~r/js-google /
+             ) == ["elem_4"]
+    end
+
+    test "css, text and many attributes criteria", state do
+      assert with_elements_map(:dom_id, state.page, [:a],
+               text: ~r/Google/,
+               class: ~r/js-google/,
+               rel: "nofollow"
+             ) == ["elem_4"]
+    end
+  end
+
+  describe ".links" do
+    test "return all links of a page", state do
+      link_ids =
+        state.page
+        |> Page.links
+        |> Enum.map(&Map.get(&1, :dom_id))
+
+        assert link_ids == ["elem_4", "elem_6", "elem_7", "elem_8"]
+    end
+  end
+end

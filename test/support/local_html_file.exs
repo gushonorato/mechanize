c2bb5c8566910ae11e076aa749fe90fa7ba8f462
Gutavo Honorato <gustavohonorato@gmail.com>
2019-06-19 19:08:28 -0300

    Mocking http requests from tests

---
+++ test/support/local_html_file.exs
@@ -0,0 +1,25 @@
+defmodule Mechanizex.HTTPAdapter.LocalHtmlFile do
+  use Mechanizex.HTTPAdapter
+  alias Mechanizex.{Request, Response, Page, Agent}
+
+  @impl Mechanizex.HTTPAdapter
+  @spec request!(pid(), Request.t()) :: Page.t()
+  def request!(agent, req) do
+    %Page{
+      response: %Response{
+        body: read_html_file(req),
+        headers: [],
+        status_code: 200,
+        url: req.url
+      },
+      request: req,
+      agent: agent,
+      parser: Agent.html_parser(agent)
+    }
+  end
+
+  defp read_html_file(req) do
+    {:ok, html } = File.read(req.url)
+    html
+  end
+end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -4,3 +4,9 @@ import Mox
 
 defmock(Mechanizex.HTMLParser.Custom, for: Mechanizex.HTMLParser)
 defmock(Mechanizex.HTTPAdapter.Custom, for: Mechanizex.HTTPAdapter)
+
+{:ok, files} = File.ls("./test/support")
+
+files
+|> Enum.filter(&(String.ends_with?(&1, [".ex",".exs"])))
+|> Enum.each(&(Code.require_file("support/#{&1}", __DIR__)))

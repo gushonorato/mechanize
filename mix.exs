a2dad2be74cfeed6c9d9dc6622d5eabec1d7a4d9
Gutavo Honorato <gustavohonorato@gmail.com>
2019-07-16 20:12:31 -0300

    Fixing typo

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mechanizex.MixProject do
   def project do
     [
       app: :mechanizex,
-      version: "0.1.0",
+      version: "0.1.0-dev",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),

2fe588e6d67078dff20f432b855ded1920118ae0
Gutavo Honorato <gustavohonorato@gmail.com>
2019-07-22 18:49:01 -0300

    Fixing typo

--- lib/mechanizex.ex
+++ lib/mechanizex.ex
@@ -1,7 +1,7 @@
 defmodule Mechanizex do
   defdelegate new(options \\ []), to: Mechanizex.Agent
   defdelegate get!(agent, url), to: Mechanizex.Agent
-  defdelegate with_form(form, criterias \\ []), to: Mechanizex.Page
+  defdelegate form_with(form, criterias \\ []), to: Mechanizex.Page
   defdelegate click_link(agent, criterias), to: Mechanizex.Page
   defdelegate fill_field(form, field, options), to: Mechanizex.Form
   defdelegate submit(form, button \\ nil), to: Mechanizex.Form

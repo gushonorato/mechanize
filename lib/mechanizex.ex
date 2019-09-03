383243b672326879808e11d65317fae39bd06301
Gutavo Honorato <gustavohonorato@gmail.com>
2019-09-03 19:00:16 -0300

    Fixing invalid delegate

--- lib/mechanizex.ex
+++ lib/mechanizex.ex
@@ -3,7 +3,6 @@ defmodule Mechanizex do
   defdelegate get!(agent, url), to: Mechanizex.Agent
   defdelegate form_with(form, criterias \\ []), to: Mechanizex.Page
   defdelegate click_link(agent, criterias), to: Mechanizex.Page
-  defdelegate fill_field(form, field, options), to: Mechanizex.Form
   defdelegate submit(form, button \\ nil), to: Mechanizex.Form
   defdelegate click_button(form, locator), to: Mechanizex.Form
 end

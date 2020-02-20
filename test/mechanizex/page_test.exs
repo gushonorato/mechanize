b84131f10e890c2e3e98d349a479bf9b31ead0a5
Gutavo Honorato <gustavohonorato@gmail.com>
2020-02-19 23:54:04 -0300

    Refactor: removing LocalPageLoader

--- test/mechanizex/page_test.exs
+++ test/mechanizex/page_test.exs
@@ -1,21 +1,18 @@
 defmodule Mechanizex.PageTest do
   use ExUnit.Case, async: true
   alias Mechanizex
-  alias Mechanizex.Test.Support.LocalPageLoader
   alias Mechanizex.Page.{Element, Link}
-  alias Mechanizex.{Page, Request, Response}
+  alias Mechanizex.{Page, Request, Response, Browser}
   import Mox
-
-  setup_all do
-    {:ok, browser: Mechanizex.Browser.new(http_adapter: :mock)}
-  end
+  import TestHelper
 
   describe ".form_with" do
-    test "return only the first form", %{browser: browser} do
-      form =
-        browser
-        |> LocalPageLoader.get("test/htdocs/two_forms.html")
-        |> Page.form_with()
+    setup do
+      stub_requests("/test/htdocs/two_forms.html")
+    end
+
+    test "return only the first form", %{page: page} do
+      form = Page.form_with(page)
 
       assert Element.attrs(form) == [
                {"id", "form-id-1"},
@@ -30,11 +27,8 @@ defmodule Mechanizex.PageTest do
              ]
     end
 
-    test "select form by its attributes", %{browser: browser} do
-      form =
-        browser
-        |> LocalPageLoader.get("test/htdocs/two_forms.html")
-        |> Page.form_with(name: "form-name-2")
+    test "select form by its attributes", %{page: page} do
+      form = Page.form_with(page, name: "form-name-2")
 
       assert Element.attrs(form) == [
                {"id", "form-id-2"},
@@ -51,44 +45,41 @@ defmodule Mechanizex.PageTest do
   end
 
   describe ".links_with" do
-    test "returns list of %Link struct", %{browser: browser} do
-      [%Link{}] =
-        browser
-        |> LocalPageLoader.get("https://htdocs.local/test/htdocs/page_with_links.html")
-        |> Page.links_with(href: ~r/google.com/)
+    setup do
+      stub_requests("/test/htdocs/page_with_links.html")
     end
 
-    test "with one attribute criteria", %{browser: browser} do
+    test "returns list of %Link struct", %{page: page} do
+      [%Link{}] = Page.links_with(page, href: ~r/google.com/)
+    end
+
+    test "with one attribute criteria", %{page: page} do
       assert(
-        browser
-        |> LocalPageLoader.get("https://htdocs.local/test/htdocs/page_with_links.html")
+        page
         |> Page.links_with(href: ~r/google.com/)
         |> Enum.map(&Element.attr(&1, :href)) == ["http://www.google.com"]
       )
     end
 
-    test "with many attributes criteria", %{browser: browser} do
+    test "with many attributes criteria", %{page: page} do
       assert(
-        browser
-        |> LocalPageLoader.get("https://htdocs.local/test/htdocs/page_with_links.html")
+        page
         |> Page.links_with(class: ~r/great-company/, rel: "search")
         |> Enum.map(&Element.attr(&1, :href)) == ["http://www.google.com"]
       )
     end
 
-    test "with text criteria", %{browser: browser} do
+    test "with text criteria", %{page: page} do
       assert(
-        browser
-        |> LocalPageLoader.get("https://htdocs.local/test/htdocs/page_with_links.html")
+        page
         |> Page.links_with(text: "Google")
         |> Enum.map(&Element.attr(&1, :href)) == ["http://www.google.com"]
       )
     end
 
-    test "multiple links with same text", %{browser: browser} do
+    test "multiple links with same text", %{page: page} do
       assert(
-        browser
-        |> LocalPageLoader.get("https://htdocs.local/test/htdocs/page_with_links.html")
+        page
         |> Page.links_with(text: ~r/Google/)
         |> Enum.map(&Element.attr(&1, :href)) == [
           "http://www.google.com",
@@ -97,10 +88,9 @@ defmodule Mechanizex.PageTest do
       )
     end
 
-    test "multiple links with same attribute", %{browser: browser} do
+    test "multiple links with same attribute", %{page: page} do
       assert(
-        browser
-        |> LocalPageLoader.get("https://htdocs.local/test/htdocs/page_with_links.html")
+        page
         |> Page.links_with(class: ~r/great-company/)
         |> Enum.map(&Element.text/1) == [
           "Google",
@@ -112,10 +102,11 @@ defmodule Mechanizex.PageTest do
       )
     end
 
-    test "image area links", %{browser: browser} do
+    test "image area links" do
+      {:ok, %{page: page}} = stub_requests("/test/htdocs/page_with_image_area_links.html")
+
       assert(
-        browser
-        |> LocalPageLoader.get("https://htdocs.local/test/htdocs/page_with_image_area_links.html")
+        page
         |> Page.links_with()
         |> Enum.map(&Element.attr(&1, :alt)) == [
           "Sun",
@@ -127,62 +118,61 @@ defmodule Mechanizex.PageTest do
   end
 
   describe ".link_with" do
-    test "returns list of %Link struct", %{browser: browser} do
+    setup do
+      stub_requests("/test/htdocs/page_with_links.html")
+    end
+
+    test "returns list of %Link struct", %{page: page} do
       %Link{} =
-        browser
-        |> LocalPageLoader.get("https://htdocs.local/test/htdocs/page_with_links.html")
+        page
         |> Page.link_with(href: ~r/google.com/)
     end
 
-    test "with one attribute criteria", %{browser: browser} do
+    test "with one attribute criteria", %{page: page} do
       assert(
-        browser
-        |> LocalPageLoader.get("https://htdocs.local/test/htdocs/page_with_links.html")
+        page
         |> Page.link_with(href: ~r/google.com/)
         |> Element.attr(:href) == "http://www.google.com"
       )
     end
 
-    test "with many attributes criteria", %{browser: browser} do
+    test "with many attributes criteria", %{page: page} do
       assert(
-        browser
-        |> LocalPageLoader.get("https://htdocs.local/test/htdocs/page_with_links.html")
+        page
         |> Page.link_with(class: ~r/great-company/, rel: "search")
         |> Element.attr(:href) == "http://www.google.com"
       )
     end
 
-    test "with text criteria", %{browser: browser} do
+    test "with text criteria", %{page: page} do
       assert(
-        browser
-        |> LocalPageLoader.get("https://htdocs.local/test/htdocs/page_with_links.html")
+        page
         |> Page.link_with(text: "Google")
         |> Element.attr(:href) == "http://www.google.com"
       )
     end
 
-    test "multiple links with same text return the first", %{browser: browser} do
+    test "multiple links with same text return the first", %{page: page} do
       assert(
-        browser
-        |> LocalPageLoader.get("https://htdocs.local/test/htdocs/page_with_links.html")
+        page
         |> Page.link_with(text: ~r/Google/)
         |> Element.attr(:href) == "http://www.google.com"
       )
     end
 
-    test "multiple links with same attribute return the first", %{browser: browser} do
+    test "multiple links with same attribute return the first", %{page: page} do
       assert(
-        browser
-        |> LocalPageLoader.get("https://htdocs.local/test/htdocs/page_with_links.html")
+        page
         |> Page.link_with(class: ~r/great-company/)
         |> Element.attr(:href) == "http://www.google.com"
       )
     end
 
-    test "image area links", %{browser: browser} do
+    test "image area links" do
+      {:ok, %{page: page}} = stub_requests("/test/htdocs/page_with_image_area_links.html")
+
       assert(
-        browser
-        |> LocalPageLoader.get("https://htdocs.local/test/htdocs/page_with_image_area_links.html")
+        page
         |> Page.link_with()
         |> Element.attr(:alt) == "Sun"
       )
@@ -192,51 +182,65 @@ defmodule Mechanizex.PageTest do
   describe ".click_link" do
     setup :verify_on_exit!
 
+    setup %{mock_request: path} do
+      expect(Mechanizex.HTTPAdapter.Mock, :request!, fn %Request{url: ^path} ->
+        %Response{
+          body: File.read(path) |> elem(1),
+          headers: [],
+          code: 200,
+          url: "http://example.com/#{path}"
+        }
+      end)
+
+      :ok
+    end
+
+    setup do
+      {:ok, browser: Mechanizex.Browser.new(http_adapter: :mock)}
+    end
+
+    @tag mock_request: "test/htdocs/page_with_links.html"
     test "click on first matched link", %{browser: browser} do
-      Mechanizex.HTTPAdapter.Mock
-      |> expect(:request, fn _, %Request{method: :get, url: "http://www.google.com"} = req ->
-        {:ok, %Page{browser: browser, request: req, response: %Response{}}}
+      expect(Mechanizex.HTTPAdapter.Mock, :request!, fn %Request{url: "http://www.google.com"} ->
+        %Response{}
       end)
 
       browser
-      |> LocalPageLoader.get("https://htdocs.local/test/htdocs/page_with_links.html")
+      |> Browser.get!("test/htdocs/page_with_links.html")
       |> Page.click_link(class: ~r/great-company/)
     end
 
+    @tag mock_request: "test/htdocs/page_with_links.html"
     test "click on first matched link by text", %{browser: browser} do
-      Mechanizex.HTTPAdapter.Mock
-      |> expect(:request, fn _, %Request{method: :get, url: "http://www.seomaster.com.br"} = req ->
-        {:ok, %Page{browser: browser, request: req, response: %Response{}}}
+      expect(Mechanizex.HTTPAdapter.Mock, :request!, fn %Request{url: "http://www.seomaster.com.br"} ->
+        %Response{}
       end)
 
       browser
-      |> LocalPageLoader.get("https://htdocs.local/test/htdocs/page_with_links.html")
+      |> Browser.get!("test/htdocs/page_with_links.html")
       |> Page.click_link("SEO Master")
     end
 
+    @tag mock_request: "test/htdocs/page_with_links.html"
     test "relative link", %{browser: browser} do
-      Mechanizex.HTTPAdapter.Mock
-      |> expect(:request, fn _, %Request{method: :get, url: "https://htdocs.local/test"} = req ->
-        {:ok, %Page{browser: browser, request: req, response: %Response{}}}
+      expect(Mechanizex.HTTPAdapter.Mock, :request!, fn %Request{url: "http://example.com/test"} ->
+        %Response{}
       end)
 
       browser
-      |> LocalPageLoader.get("https://htdocs.local/test/htdocs/page_with_links.html")
+      |> Browser.get!("test/htdocs/page_with_links.html")
       |> Page.click_link("Back")
     end
 
+    @tag mock_request: "test/htdocs/page_with_image_area_links.html"
     test "image area links", %{browser: browser} do
       Mechanizex.HTTPAdapter.Mock
-      |> expect(:request, fn _,
-                             %Request{
-                               method: :get,
-                               url: "https://htdocs.local/test/htdocs/sun.html"
-                             } = req ->
-        {:ok, %Page{browser: browser, request: req, response: %Response{}}}
+      |> expect(:request!, fn %Request{url: "http://example.com/test/htdocs/sun.html"} ->
+        %Response{}
       end)
 
       browser
-      |> LocalPageLoader.get("https://htdocs.local/test/htdocs/page_with_image_area_links.html")
+      |> Browser.get!("test/htdocs/page_with_image_area_links.html")
       |> Page.click_link(alt: "Sun")
     end
   end

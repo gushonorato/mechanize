8f35046ee9a6a39bdf46c2eb3bae4aaba33385a3
Gutavo Honorato <gustavohonorato@gmail.com>
2020-06-01 21:20:32 -0300

    Docs

--- lib/mechanize/page.ex
+++ lib/mechanize/page.ex
@@ -1,30 +1,84 @@
 defmodule Mechanize.Page do
+  @moduledoc """
+  The HTML Page.
+
+  This module defines Mechanize.Page and the main functions for working with Pages.
+
+  The Page is created as a result of a successful HTTP request.
+  ```
+  alias Mechanize.{Browser, Page}
+
+  browser = Browser.new()
+  page = Browser.get!(browser, "https://www.example.com")
+  ```
+  """
   alias Mechanize.{Response, Query, Form}
   alias Mechanize.Page.{Link, Element}
 
-  defstruct response_chain: nil, status_code: nil, content: nil, url: nil, browser: nil, parser: nil
+  defstruct [:response_chain, :status_code, :content, :url, :browser, :parser]
 
   @type t :: %__MODULE__{
           response_chain: [Response.t()],
           status_code: integer(),
           content: String.t(),
           url: String.t(),
-          browser: pid(),
+          browser: Browser.t(),
           parser: module()
         }
 
   defmodule ClickError do
+    @moduledoc """
+    Raised when an error occurs on a click action.
+    """
     defexception [:message]
   end
 
   defmodule InvalidMetaRefreshError do
+    @moduledoc """
+    Raised when Mechanize can not parse the `content` attribute of a
+    `<meta http-equiv="refresh" ...>` element inside the page content.
+    """
     defexception [:message]
   end
 
+  @doc """
+  Returns the browser that fetched the `page`.
+  """
+  @spec browser(Page.t()) :: Browser.t()
   def browser(page), do: page.browser
+
+  @doc """
+  Returns the `page` url.
+  """
+  @spec url(Page.t()) :: String.t()
   def url(page), do: page.url
+
+  @doc """
+  Returns the page content.
+  """
+  @spec content(Page.t()) :: String.t()
   def content(page), do: page.content
 
+  @doc """
+  Extracts meta-refresh data from a `page`.
+
+  A two element tuple with a integer representing the delay in the first position and
+  the a string representing the URL in the second position will be returned if a
+  `<meta http-equiv="refresh" ...>` is found, otherwise `nil` will be returned.
+
+  Raises `Mechanize.Page.InvalidMetaRefreshError` if Mechanize cannot parse the `content` attribute
+  of the meta-refresh.
+
+  ## Example
+  ```
+  # <meta http-equiv="refresh" content="10; url=https://www.example.com">
+  {delay, url} = Page.meta_refresh(page)
+
+  delay # => 10
+  url # => https://www.example.com
+  ```
+  """
+  @spec meta_refresh(Page.t()) :: {integer(), String.t()}
   def meta_refresh(nil), do: raise(ArgumentError, "page is nil")
 
   def meta_refresh(page) do

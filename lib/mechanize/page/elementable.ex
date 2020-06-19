1db3a2969c40f3f0253425ceee9531390053c75c
Gutavo Honorato <gustavohonorato@gmail.com>
2020-06-18 23:20:08 -0300

    Docs

--- lib/mechanize/page/elementable.ex
+++ lib/mechanize/page/elementable.ex
@@ -1,9 +1,33 @@
 defprotocol Mechanize.Page.Elementable do
-  @moduledoc false
+  @moduledoc """
+  Protocol used to extract element data using `Mechanize.Page.Element` module.
 
-  def element(e)
+  To implement this protocol you have to implement a single function
+  `Mechanize.Page.Elementable.element/1`.
+
+  ## Example
+
+  Implementation of Elementable for a `Mechanize.Form`:
+
+  ```
+  defmodule Mechanize.Form do
+    defstruct element: nil,
+              fields: []
+  end
+
+  defimpl Mechanize.Page.Elementable, for: Mechanize.Form do
+    def element(e), do: e.element
+  end
+  ```
+  """
+
+  @doc """
+  Returns an `Mechanize.Page.Element` struct from elementable data.
+  """
+  @spec element(any) :: Element.t()
+  def element(elementable)
 end
 
 defimpl Mechanize.Page.Elementable, for: Any do
-  def element(e), do: e.element
+  def element(elementable), do: elementable.element
 end
